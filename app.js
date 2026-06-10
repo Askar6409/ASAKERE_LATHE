9ba927695338d3d3ab77126feb2cf69eae39e24f
ASKAR <74613512+Askar6409@users.noreply.github.com>
2026-06-10 22:34:04 +0330

    Js

--- app.js
+++ app.js
@@ -61,8 +61,8 @@ function getTotal() {
 function buildSmsText() {
   const selected = getSelectedServices();
 
-  let text = `تراشکاری عساکره آ\n`;
-  text += `فاکتور آ\n`;
+  let text = `تراشکاری عساکره \n`;
+  text += `فاکتور \n`;
   text += `تاریخ: ${state.date}\n`;
   text += `نام مشتری: ${state.customerName || "-"}\n`;
   text += `----------------------\n`;
